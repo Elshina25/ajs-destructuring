fe5c3a474fbd85c10a1eb7efbe8292a02fca2d32
Анастасия Ельшина <salnaya92@gmail.com>
2023-03-15 13:48:31 +0300

    project is done

--- src/js/__tests__/test.func.js
+++ src/js/__tests__/test.func.js
@@ -1,2 +1,43 @@
-import destructuring from "../app";
+import destructuring from '../app';
 
+test('destructuring player object', () => {
+  const character = {
+    name: 'Лучник',
+    type: 'Bowman',
+    health: 50,
+    level: 3,
+    attack: 40,
+    defence: 10,
+    special: [
+      {
+        id: 8,
+        name: 'Двойной выстрел',
+        icon: 'http://...',
+        description: 'Двойной выстрел наносит двойной урон',
+      },
+      {
+        id: 9,
+        name: 'Нокаутирующий удар',
+        icon: 'http://...',
+      },
+    ],
+  };
+
+  const result = destructuring(character);
+  expect(result).toEqual(
+    [
+      {
+        id: 8,
+        name: 'Двойной выстрел',
+        icon: 'http://...',
+        description: 'Двойной выстрел наносит двойной урон',
+      },
+      {
+        id: 9,
+        name: 'Нокаутирующий удар',
+        icon: 'http://...',
+        description: 'Описание недоступно',
+      },
+    ],
+  );
+});
